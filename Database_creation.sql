96f2e8ec10152a4bda21e660b1d35e88a6e846f6
adarsh-kishore786 <adarsh.kishore786@gmail.com>
2022-04-12 12:57:26 +0530

    Some changes to DB

--- Database_creation.sql
+++ Database_creation.sql
@@ -1,8 +1,6 @@
 create database Airport;
 use Airport;
-show tables;
 
--- Create general Person table
 create table Person (
 	Name varchar(20) not null,
     DOB date not null,
@@ -10,54 +8,58 @@ create table Person (
     Address varchar(30),
     Contact_no int(10) NOT NULL
 );
-desc Person;
-show create table Person;
 
 create table Pilot (
 	Pilot_id varchar(10) primary key,
-    Flight_id varchar(10)
+	Name varchar(20) not null,
+    DOB date not null,
+    Gender char(1),
+    Address varchar(30),
+    Contact_no int(10) NOT NULL
 );
 
 create table Staff (
 	Staff_id varchar(10) primary key,
-    Flight_id varchar(10)
+    Name varchar(20) not null,
+    DOB date not null,
+    Gender char(1),
+    Address varchar(30),
+    Contact_no int(10) NOT NULL
 );
 
 create table Passenger (
 	Passenger_id varchar(10) primary key,
-    Flight_id varchar(10),
-    Seat_No varchar(5)
+    Name varchar(20) not null,
+    DOB date not null,
+    Gender char(1),
+    Address varchar(30),
+    Contact_no int(10) NOT NULL,
+    Ticket_id varchar(10)
 );
-desc Passenger;
 
 create table Terminal (
-	Passenger_id varchar(10),
-    Flight_id varchar(10),
-    Boarding_time TIME not null,
-    terminal_no char(5) not null
+	Terminal_no varchar(5) primary key,
+	Ticket_id varchar(10),
+    Boarding_time time not null
 );
 
 create table Ticket (
-	Passenger_id varchar(10),
+	Ticket_id varchar(10) primary key,
     Flight_id varchar(10),
     Flight_class varchar(10) not null
 );
 
 create table Flight (
-    Flight_id varchar(10),
+    Flight_id varchar(10) primary key,
     Pilot_id varchar(10),
     Staff_id varchar(10),
     Passenger_id varchar(10),
     From_city varchar(20) not null,
     To_city varchar(20) not null,
     Departure_time time not null,
-    Arrival_time time not null
+    Arrival_time time not null,
+    Terminal_no varchar(5)
 );
-
-alter table Terminal
-add constraint FK_Terminal_Passenger foreign key (Passenger_id) references Passenger(Passenger_id);
-
-show create table Terminal;
 
 
 
