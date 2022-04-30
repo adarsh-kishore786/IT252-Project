7add7c08ed586a5a5d14c8e34f5c113d2c8822c2
adarsh-kishore786 <adarsh.kishore786@gmail.com>
2022-04-30 15:18:41 +0530

    New improved DB thanks to Ayman

--- Complex_Queries.sql
+++ Complex_Queries.sql
@@ -7,8 +7,8 @@ SELECT Staff_id,Flight_id, Name as Staff_name from Staff where Staff.Flight_id i
 -- Select Passenger Details who will have to board planes with departure_time in the morning
 SELECT * from Passenger where Passenger_id in (SELECT Passenger_id from Ticket where Ticket.Flight_id in (SELECT distinct(Flight.Flight_id) FROM Flight WHERE Departure_time < "12:00:00"));
 
--- Find the bifurcation between number of morning and evening tickets 
-SELECT * FROM 
+-- Find the bifurcation between number of morning and evening tickets
+SELECT * FROM
 (SELECT count(*) as TicketsInEvening from Ticket where Ticket.Flight_id in (SELECT distinct(Flight.Flight_id) FROM Flight WHERE Departure_time >= "12:00:00")) EveningTickets ,
-(SELECT count(*) as TicketsInMorning from Ticket where Ticket.Flight_id in (SELECT distinct(Flight.Flight_id) FROM Flight WHERE Departure_time < "12:00:00")) MorningTickets, 
+(SELECT count(*) as TicketsInMorning from Ticket where Ticket.Flight_id in (SELECT distinct(Flight.Flight_id) FROM Flight WHERE Departure_time < "12:00:00")) MorningTickets,
 (SELECT count(*) as TotalTickets FROM Ticket)  TotalTickets;
