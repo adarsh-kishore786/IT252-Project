233f54b91e11812d0c7198a9172a90a17ef3496f
Ayman <76606666+Ayman161803@users.noreply.github.com>
2022-04-30 12:17:11 +0530

    improved database schema

--- Database_creation.sql
+++ Database_creation.sql
@@ -3,7 +3,8 @@ use Airport;
 
 create table Pilot (
 	Pilot_id varchar(10) primary key,
-	Name varchar(20) not null,
+    Flight_id varchar(10),
+    Name varchar(20) not null,
     DOB date not null,
     Gender char(1),
     Address varchar(30),
@@ -12,6 +13,7 @@ create table Pilot (
 
 create table Staff (
 	Staff_id varchar(10) primary key,
+    Flight_id varchar(10),
     Name varchar(20) not null,
     DOB date not null,
     Gender char(1),
@@ -32,34 +34,23 @@ create table Terminal (
 	Terminal_no varchar(5),
 	Ticket_id varchar(10),
     Boarding_time time not null,
-    primary key (Terminal_no, Ticket_id, Boarding_time)
+    primary key (Terminal_no, Ticket_id)
 );
 
 create table Ticket (
 	Ticket_id varchar(10) primary key,
+    Flight_id varchar(10),
     Passenger_id varchar(10),
     Flight_class varchar(10) not null
 );
 
 create table Flight (
     Flight_id varchar(10) primary key,
-    Pilot_id varchar(10),
-    Staff_id varchar(10),
-    Passenger_id varchar(10),
     From_city varchar(20) not null,
     To_city varchar(20) not null,
     Departure_time time not null,
     Arrival_time time not null
 );
-
-alter table Flight
-add constraint FK_Flight_Pilot foreign key (Pilot_id) references Pilot(Pilot_id);
-
-alter table Flight
-add constraint FK_Flight_Passenger foreign key (Passenger_id) references Passenger(Passenger_id);
-
-alter table Flight
-add constraint FK_Flight_Staff foreign key (Staff_id) references Staff(Staff_id);
 
 alter table Ticket
 add constraint FK_Ticket_Passenger foreign key (Passenger_id) references Passenger(Passenger_id); 
@@ -67,3 +58,12 @@ add constraint FK_Ticket_Passenger foreign key (Passenger_id) references Passeng
 alter table Terminal
 add constraint FK_Terminal_Ticket foreign key (Ticket_id) references Ticket (Ticket_id);
 
+
+alter table Staff
+add constraint FK_Staff_Flight foreign key (Flight_id) references Flight (Flight_id);
+
+alter table Pilot
+add constraint FK_Pilot_Flight foreign key (Flight_id) references Flight (Flight_id);
+
+alter table Ticket
+add constraint FK_Ticket_Flight foreign key (Flight_id) references Flight (Flight_id);
