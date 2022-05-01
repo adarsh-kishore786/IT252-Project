f10689298fcadfa24f18ce09b74db54756939d36
Ayman <76606666+Ayman161803@users.noreply.github.com>
2022-05-01 12:53:23 +0530

    Query strings were in wrong format

--- SimpleQueries.sql
+++ SimpleQueries.sql
@@ -14,16 +14,16 @@ SELECT DISTINCT Terminal_no FROM Terminal WHERE Boarding_time > '09:00:00';
 SELECT COUNT(Passenger_id) FROM Ticket WHERE Flight_class = 'FirstClass';
 
 -- display staff id, name of staff and flight id of flight FL003
-SELECT Staff_id, Flight_id, Name FROM Staff WHERE Flight_id = “FL003”;
+SELECT Staff_id, Flight_id, Name FROM Staff WHERE Flight_id = 'FL003';
 
 --display passenger id and flight id of passengers travelling in first class
-SELECT Passenger_id, Flight_id FROM Ticket WHERE Flight_class = “Economy”;
+SELECT Passenger_id, Flight_id FROM Ticket WHERE Flight_class = 'Economy';
 
 --display number of female passengers
-SELECT COUNT(Passenger_id) FROM Passenger WHERE gender = “F”;
+SELECT COUNT(Passenger_id) FROM Passenger WHERE gender = 'F';
 
 --display number of flights arriving in mangalore
-select COUNT(Flight_id) FROM Flight WHERE to_city = “Mangalore”;
+select COUNT(Flight_id) FROM Flight WHERE to_city = 'Mangalore';
 
 --display details of pilots who are below the age of 45
 SELECT * FROM Pilot WHERE YEAR(CURRENT_DATE()) - YEAR(DOB) <= 45;
